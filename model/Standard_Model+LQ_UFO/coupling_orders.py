0df1010a304f27a7e85ce8500dc31f7cee7e197c
Yuta Takahashi <Yuta.Takahashi@cern.ch>
2017-10-27 10:15:06 +0200

    add UFO

---
+++ model/Standard_Model+LQ_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 1.6.16
+# Mathematica version: 8.0 for Mac OS X x86 (64-bit) (October 5, 2011)
+# Date: Wed 2 Oct 2013 12:41:41
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
